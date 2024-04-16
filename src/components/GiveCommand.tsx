d2a5e032aa9ae77a4543bd5b9a0cc7fac4664d04
stbn1699 <stbnbasson@gmail.com>
2024-04-16 10:33:43 +0200

    initialisation

--- src/components/GiveCommand.tsx
+++ src/components/GiveCommand.tsx
@@ -1,13 +1,15 @@
 import ButtonsJavaEdition from "./utilities/ButtonsJavaEdition";
 import "../styles/GiveCommand.css"
 
-function Home() {
+function GiveCommand() {
 
     return (
         <div className="give-command">
 
             <select name="item" id="item">
                 <option value="sword">Sword</option>
+                <option value="Pickaxe">Pickaxe</option>
+                <option value="Shovel">Shovel</option>
             </select>
 
             <ButtonsJavaEdition taille="1" title="Retour" path="goback"/>
@@ -16,4 +18,4 @@ function Home() {
 
 }
 
-export default Home
+export default GiveCommand
