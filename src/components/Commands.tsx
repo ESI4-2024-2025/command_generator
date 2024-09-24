68532e6000d394a722a7981def8d38bc8e91db72
Plexi <stbnbasson@gmail.com>
2024-09-24 12:09:34 +0200

    feat: refonte de la lsite des commandes

--- src/components/Commands.tsx
+++ src/components/Commands.tsx
@@ -1,19 +1,20 @@
 import ButtonsJavaEdition from "./utilities/ButtonsJavaEdition";
-import "../styles/Commands.css"
+import "../styles/Commands.css";
 
 function Commands() {
-
-    return (
-        <div className="commands" data-testid="Home">
-
-            <div className="commands-buttons">
-                <ButtonsJavaEdition title="GIVE"
-                    taille="19"
-                    path="give" />
-            </div>
-        </div>
-    )
-
+	return (
+		<div className="commands-page">
+			<h1 className="commands-title">Liste Des Commandes</h1>
+			<div className="commands" data-testid="Home">
+				<div className="commands-buttons">
+					<ButtonsJavaEdition title="/GIVE" taille="35" path="give"/>
+				</div>
+			</div>
+			<div className="back-button">
+				<ButtonsJavaEdition taille="20" title="Retour" path="goback"/>
+			</div>
+		</div>
+	);
 }
 
-export default Commands
+export default Commands;
