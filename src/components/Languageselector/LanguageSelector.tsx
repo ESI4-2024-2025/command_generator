43732917845ccbe3c3f96f5ca2cad1c844ed413d
Plexi <stbnbasson@gmail.com>
2024-10-22 14:32:33 +0200

    feat: se rappeler de la langue sélectionnée

--- src/components/Languageselector/LanguageSelector.tsx
+++ src/components/Languageselector/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useTranslation} from "react-i18next";
 import "../../styles/LanguageSelector.css";
 
@@ -9,15 +9,29 @@ interface LanguageSelectorProps {
 const LanguageSelector: React.FC<LanguageSelectorProps> = ({setLanguage}) => {
 	const {i18n} = useTranslation();
 
+	/**
+	 * Change the language of the website
+	 *
+	 * @param lng The language to change to
+	 */
 	const changeLanguage = (lng: string) => {
 		i18n.changeLanguage(lng);
 		setLanguage(lng);
+		localStorage.setItem("language", lng);
 	};
+
+	/**
+	 * Set the language to the saved language in the local storage everytime the component is mounted
+	 */
+	useEffect(() => {
+		const savedLanguage = localStorage.getItem("language") || "en";
+		i18n.changeLanguage(savedLanguage);
+		setLanguage(savedLanguage);
+	}, []);
 
 	return (
 		<div className="language-selector">
-			<select onChange={(e) => changeLanguage(e.target.value)}
-					defaultValue={i18n.language}>
+			<select onChange={(e) => changeLanguage(e.target.value)} defaultValue={localStorage.getItem("language") || "en"}>
 				<option value="en">English</option>
 				<option value="fr">Français</option>
 			</select>
